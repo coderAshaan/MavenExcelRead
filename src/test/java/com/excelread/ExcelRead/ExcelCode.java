e43961015ca1baef043cc951a9364f43829302b5
USER <USER@DESKTOP-A136ETG>
2023-07-04 16:34:01 +0530

    Excel Read Commit

---
+++ src/test/java/com/excelread/ExcelRead/ExcelCode.java
@@ -0,0 +1,45 @@
+package com.excelread.ExcelRead;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+//Test
+public class ExcelCode {
+	
+	public static FileInputStream f;
+	public static XSSFWorkbook w;
+	public static XSSFSheet s;
+	
+	public static String readStringData(int i,int j) throws IOException
+	{
+		f = new FileInputStream("C:\\Users\\USER\\Desktop\\ExcelRead\\Recordsone.xlsx");
+		w = new XSSFWorkbook(f);
+		s = w.getSheet("Sheet1");
+		
+		Row r = s.getRow(i);
+		Cell c = r.getCell(j);
+		
+		return c.getStringCellValue();	
+	}
+	
+	public static double readIntegerData(int i, int j) throws IOException
+	{
+		f = new FileInputStream("C:\\Users\\USER\\Desktop\\ExcelRead\\Recordsone.xlsx");
+		w = new XSSFWorkbook(f);
+		s = w.getSheet("Sheet1");
+		
+		Row r = s.getRow(i);
+		Cell c = r.getCell(j);
+		
+		return c.getNumericCellValue()	;
+	}
+	
+
+}
---
+++ src/test/java/com/excelread/ExcelRead/ExcelMain.java
@@ -0,0 +1,15 @@
+package com.excelread.ExcelRead;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println(ExcelCode.readStringData(1, 1));
+		System.out.println(ExcelCode.readIntegerData(2, 0));
+	
+
+	}
+
+}
